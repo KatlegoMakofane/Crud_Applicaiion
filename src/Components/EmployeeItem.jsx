e044d8ee79103d4478a572fee6a1258a62f587e8
KatlegoMakofane <87699446+KatlegoMakofane@users.noreply.github.com>
2023-10-10 12:30:03 +0200

    Update EmployeeItem.jsx

--- src/Components/EmployeeItem.jsx
+++ src/Components/EmployeeItem.jsx
@@ -3,9 +3,11 @@ import { removeEmployee } from '../service/localStorage';
 import { getListEmployees } from '../service/localStorage';
 import { useNavigate } from 'react-router-dom';
 import "./EmployeeForm.css";
-
+import {MdOutlineDelete} from 'react-icons/md'
+import {FiEdit2} from 'react-icons/fi'
 const EmployeeItem = ({ employee, setEmployees }) => {
-    const { id, name, email, image} = employee;
+    const { id, name, email, fileurl} = employee;
+    console.log(employee);
     const navigate = useNavigate();
 
     const deleteEmployee = () => {
@@ -13,29 +15,42 @@ const EmployeeItem = ({ employee, setEmployees }) => {
         setEmployees(getListEmployees());
     }
   return (
-    <tr className="table-primasry">
-            <td><img src={image} className='img' alt=""/></td>
-            <tr className='tr'>
-            <td>{name}</td>
-            </tr>
 
-            <tr className='tr'>
-            <td>{email}</td>
-            </tr>
-            
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td className='trd'>
-                <div className="d-flex gap-3">
-                    <span type="button" className="badge bg-success" onClick={() => navigate(`/edit-employee/${id}`)}>Edit</span>
-                    <span type="button" className="badge bg-danger" onClick={() => deleteEmployee()}>Delete</span>
-                </div>
-            </td>
-        </tr>
-  )
+    <div  className='Member__container'>
+        <div className='leftside'>
+            <img src={fileurl} className='img' alt=""/>
+               <div className='text__container'>
+                    <p>{name}</p>
+                   <br></br>
+                   <p>{email}</p>
+               </div>
+                 
+              
+        </div>
+       
+        <div className='rightside'> 
+                  <span type="button" className="badge bg-success" onClick={() => navigate(`/edit-employee/${id}`)}><FiEdit2 style={{color: 'blue', fontSize: "30"}}/></span>
+                   <span type="button" className="badge bg-danger" onClick={() => deleteEmployee()}><MdOutlineDelete style={{color: 'blue', fontSize: "30"}}/></span>
+              </div>
+        
+                  
+         {/* <img src={image} className='img' alt=""/>
+           
+           {name}
+          
+
+           
+           {email}
+           
+               <div className="d-flex gap-3">
+                   <span type="button" className="badge bg-success" onClick={() => navigate(`/edit-employee/${id}`)}>Edit</span>
+                   <span type="button" className="badge bg-danger" onClick={() => deleteEmployee()}>Delete</span>
+               </div> */}
+    </div>
+    
+          
+        
+  ) 
 }
 
 export default EmployeeItem
